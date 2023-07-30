d589282390cf0a9598164060af742e5e52ad2e19
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-07-30 18:33:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function decimalToBinary(num) {
   // Write your code here
-	return decimal.toString(num);
+	return decimalToBinary(num);
 	console.log(decimalToBinary(7));
 	console.log(decimalToBinary(10));
 	console.log(decimalToBinary(33));
